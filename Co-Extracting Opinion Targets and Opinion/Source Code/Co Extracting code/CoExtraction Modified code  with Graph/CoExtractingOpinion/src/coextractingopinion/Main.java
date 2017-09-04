7bf34114a52260a11ee01baeac41cb9c6a006df8
rajesh <rajeshsai.kammari@gmail.com>
2017-09-04 16:06:25 +0530

    my be 4th year

---
+++ Co-Extracting Opinion Targets and Opinion/Source Code/Co Extracting code/CoExtraction Modified code  with Graph/CoExtractingOpinion/src/coextractingopinion/Main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package coextractingopinion;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author SRAVAN
+ */
+public class Main {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        System.out.println("Here");
+        try
+        {                    
+            UIManager.setLookAndFeel("com.jtattoo.plaf.smart.SmartLookAndFeel");                         
+            
+            HomePage hp=new HomePage();
+            hp.setResizable(false);
+            hp.setTitle("Home Page");
+            hp.setVisible(true);  
+        }
+	catch (Exception ex)
+	{
+            System.out.println("Failed loading L&F: ");
+            System.out.println(ex);
+	}        
+    }
+}
